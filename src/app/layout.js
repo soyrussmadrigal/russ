2c4ad2639be77fcf3d1dacb8b195b29bc5261deb
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2024-08-02 12:54:27 -0600

    Update website title and description

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,8 +9,8 @@ import AuthProvider from "@/providers/AuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Lama Dev Blog App",
-  description: "The best blog app!",
+  title: "Russ Madrigal",
+  description: "Sitio web Oficial",
 };
 
 export default function RootLayout({ children }) {
